d54871d5905a602668a63345087c008d34df6ad7
Omar Shamsul Haque <omarhaque7484@gmail.com>
2023-07-24 11:46:51 +0600

    Commit 5
    Post-deploy error fix

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -24,7 +24,7 @@ const Index = ({ orders, products }) => {
         const currentStatus = item.status;
 
         try {
-            const res = await axios.put("http://localhost:3000/api/orders/" + id, {status: currentStatus + 1});
+            const res = await axios.put("https://cheezapp.onrender.com/api/orders/" + id, {status: currentStatus + 1});
             setOrderList([
                 res.data, 
                 ...orderList.filter((order) => order._id !== id)
